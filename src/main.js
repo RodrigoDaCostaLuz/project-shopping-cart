4160ead97861100f991863a65f22fdc090c3c78d
RodrigoDaCostaLuz <rodrigoluz999@gmail.com>
2023-02-15 17:10:02 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
 import { searchCep } from './helpers/cepFunctions';
 import './style.css';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+(async () => {
+  try {
+    const searchProduct = 'computador';
+    const productList = await fetchProductsList(searchProduct);
+    const productsContainer = document.querySelector('.products');
+    productList.forEach((product) => {
+      const productElement = createProductElement(product);
+      productsContainer.appendChild(productElement);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
