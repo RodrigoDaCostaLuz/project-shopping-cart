c94a80ba7b9d17f0a1334005db0d47b2945b8e50
RodrigoDaCostaLuz <rodrigoluz999@gmail.com>
2023-02-18 17:11:11 -0300

    refatora código do arquivo main para fazer o requisito 9

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,34 @@
 export const getAddress = () => {
   // seu código aqui
+  // const endpoints = [
+  //   `https://cep.awesomeapi.com.br/json/${cep}`,
+  //   `https://brasilapi.com.br/api/cep/v2/${cep}`,
+  // ];
+
+  // const responses = await Promise.any(endpoints.map((endpoint) => fetch(endpoint)));
+
+  // const json = await responses.json();
+
+  // if (json.cep) {
+  //   return `${json.logradouro} - ${json.bairro} - ${json.localidade} - ${json.uf}`;
+  // } throw new Error('CEP não encontrado');
 };
 
 export const searchCep = () => {
   // seu código aqui
+  // const cep = document.querySelector('.cep-input').value.replace(/\D/g, '');
+
+  // if (cep.length !== 8) {
+  //   return;
+  // }
+
+  // try {
+  //   const address = await getAddress(cep);
+
+  //   const addressElement = document.querySelector('.cart__address');
+  //   addressElement.textContent = address;
+  // } catch (error) {
+  //   const addressElement = document.querySelector('.cart__address');
+  //   addressElement.textContent = 'CEP não encontrado';
+  // }
 };
--- src/main.js
+++ src/main.js
@@ -1,73 +1,70 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
-import { createProductElement, createCustomElement, createCartProductElement } from './helpers/shopFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { createProductElement, createCustomElement,
+  createCartProductElement } from './helpers/shopFunctions';
+import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
+import './style.css';
 
-// Elementos do DOM
 const produtos = document.querySelector('.products');
 const btnProdutos = document.getElementsByClassName('product__add');
 const carrinhoCompras = document.querySelector('.cart__products');
+const precosTotais = document.querySelector('.total-price');
 
-// Event listener para buscar CEP
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
-// Função para exibir mensagem personalizada
 const msgCustom = (element, className, innerText = '') => {
   const customElement = createCustomElement(element, className, innerText);
   produtos.appendChild(customElement);
 };
 
-// Função para remover elemento de carregamento
-const removeLoading = () => {
-  const elementToDelete = document.querySelector('.loading');
-  elementToDelete.remove();
-};
-
-// Função para adicionar produto ao carrinho de compras
 const addCompras = (product) => {
   const productId = product.querySelector('.product__id').textContent;
   saveCartID(productId);
   fetchProduct(productId).then((productData) => {
     const cartProductElement = createCartProductElement(productData);
     carrinhoCompras.appendChild(cartProductElement);
+    const precos = Array.from(document.querySelectorAll('.cart__product-price'))
+      .map((price) => Number(price.innerText));
+    const precoSomados = precos.reduce((acc, curr) => acc + curr, 0);
+    precosTotais.innerHTML = precoSomados.toFixed(2);
   });
 };
 
-// Função para adicionar eventos de clique nos botões de adicionar produto
-const addComprasE = () => {
+const addComprasX = () => {
   Array.from(btnProdutos).forEach((product) => {
     product.addEventListener('click', () => addCompras(product.parentNode));
   });
 };
 
-// Função para exibir produtos na página
 const displayProducts = async (searchTerm) => {
-  // Exibe mensagem de carregamento
   msgCustom('p', 'loading', 'carregando...');
-
   try {
     const results = await fetchProductsList(searchTerm);
     if (results.length) {
-      // Adiciona cada produto retornado pela API na página
       results.forEach((item) => {
         produtos.appendChild(createProductElement(item));
       });
-      // Adiciona eventos de clique nos botões de adicionar produto
-      addComprasE();
+      addComprasX();
     } else {
-      // Exibe mensagem de erro caso não haja resultados
       msgCustom('p', 'error', 'Nenhum resultado encontrado.');
     }
   } catch (error) {
-    // Exibe mensagem de erro caso ocorra um erro ao buscar produtos
     msgCustom('p', 'error', 'Algum erro ocorreu, recarregue a página e tente novamente.');
   } finally {
-    // Remove mensagem de carregamento
-    removeLoading();
+    const salvaProdutos = getSavedCartIDs();
+    const promises = salvaProdutos.map((product) => fetchProduct(product));
+    Promise.all(promises).then((result) => {
+      result.forEach((resul) => {
+        const productElement = createCartProductElement(resul);
+        carrinhoCompras.appendChild(productElement);
+      });
+      const precos = result.map((resul) => resul.price);
+      const precoSomados = precos.reduce((acc, curr) => acc + curr, 0);
+      precosTotais.innerHTML = precoSomados.toFixed(2);
+    });
   }
 };
 
-// Exibe os produtos na página assim que ela é carregada
 window.onload = () => {
   displayProducts('computador');
 };
